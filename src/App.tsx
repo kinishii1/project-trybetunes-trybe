0993ebbe7ee5553f71a162ee0c4f68689a7484b2
Kin Ishii <kinishii97@gmail.com>
2023-10-15 02:52:26 +0900

    linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { Route, Routes } from "react-router-dom";
-import Login from "./pages/Login";
-import Search from "./pages/Search";
-import Album from "./pages/Album";
-import PageLayout from "./pages/PageLayout";
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import PageLayout from './pages/PageLayout';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/" element={<PageLayout />}>
-        <Route path="/search" element={<Search />} />
-        <Route path="/album/:id" element={<Album />} />
+      <Route path="/" element={ <Login /> } />
+      <Route path="/" element={ <PageLayout /> }>
+        <Route path="/search" element={ <Search /> } />
+        <Route path="/album/:id" element={ <Album /> } />
       </Route>
       {/* <Route path='/favorites' element={<Favorites />} />
       <Route path='/profile' element={<Profile />} />
