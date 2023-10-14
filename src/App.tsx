cdf7720c11568de5d891bb02219796b05569cc5e
Kin Ishii <kinishii97@gmail.com>
2023-10-15 02:30:21 +0900

    implementando Header e PageLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { Route, Routes } from 'react-router-dom';
-import Login from './pages/Login';
-import Search from './pages/Search';
-import Album from './pages/Album';
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Search from "./pages/Search";
+import Album from "./pages/Album";
+import PageLayout from "./pages/PageLayout";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={ <Login /> } />
-      <Route path="/search" element={ <Search /> } />
-      <Route path="/album/:id" element={ <Album /> } />
+      <Route path="/" element={<Login />} />
+      <Route path="/" element={<PageLayout />}>
+        <Route path="/search" element={<Search />} />
+        <Route path="/album/:id" element={<Album />} />
+      </Route>
       {/* <Route path='/favorites' element={<Favorites />} />
       <Route path='/profile' element={<Profile />} />
       <Route path='/profile/edit' element={<ProfileEdit />} />
---
+++ src/components/Header.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import { UserType } from '../types';
+
+function Header() {
+  const [user, setUser] = useState<UserType | undefined>(undefined);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      setIsLoading(true);
+      const response = await getUser();
+      setUser(response);
+      console.log(user);
+      setIsLoading(false);
+    };
+    fetchUser();
+  }, []);
+
+  if (isLoading) return <p>Carregando...</p>;
+
+  return (
+    <header data-testid="header-component">
+      {user && <h1 data-testid="header-user-name">{user.name}</h1> }
+      <nav>
+        <NavLink data-testid="link-to-search" to="/search">
+          Home
+        </NavLink>
+        <NavLink data-testid="link-to-favorites" to="/favorites">
+          Home
+        </NavLink>
+        <NavLink data-testid="link-to-profile" to="/profile">
+          Home
+        </NavLink>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
