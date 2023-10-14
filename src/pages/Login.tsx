07af379e8b735722467b4d27011294fd52e55285
Kin Ishii <kinishii97@gmail.com>
2023-10-14 23:40:42 +0900

    pagina login

---
+++ src/pages/Login.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { createUser } from '../services/userAPI';
+
+function Login() {
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [name, setName] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+    setIsDisabled(event.target.value.length < 3);
+  };
+
+  const submitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+    setIsLoading(true);
+    event.preventDefault();
+    const creatingUser = await createUser({ name });
+
+    if (creatingUser) {
+      setIsLoading(false);
+    }
+  };
+
+  if (isLoading) return <p>Carregando...</p>;
+
+  return (
+    <form onSubmit={ submitHandler }>
+      <input
+        data-testid="login-name-input"
+        value={ name }
+        type="text"
+        onChange={ (e) => changeHandler(e) }
+      />
+      <button
+        disabled={ isDisabled }
+        data-testid="login-submit-button"
+        type="submit"
+      >
+        Entrar
+      </button>
+    </form>
+  );
+}
+
+export default Login;
